32faaeb1ea0bee2fda0e6864e8ab005151e7afaf
kowi.kowi <kowi.db.kowi@gmail.com>
2025-02-01 15:40:33 +0100

    test

---
+++ project1/target/debug/deps/project1-00e7d58f87d42d74.d
@@ -0,0 +1,9 @@
+/home/marylka/Desktop/rust_learning/project1/target/debug/deps/libproject1-00e7d58f87d42d74.rmeta: src/main.rs Cargo.toml
+
+/home/marylka/Desktop/rust_learning/project1/target/debug/deps/project1-00e7d58f87d42d74.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ project1/target/debug/deps/project1-1b16773ad7eeee5e.d
@@ -0,0 +1,9 @@
+/home/marylka/Desktop/rust_learning/project1/target/debug/deps/libproject1-1b16773ad7eeee5e.rmeta: src/main.rs Cargo.toml
+
+/home/marylka/Desktop/rust_learning/project1/target/debug/deps/project1-1b16773ad7eeee5e.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
