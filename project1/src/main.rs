08d94378e6f50d20da60816b8b30ab6dc393b4e0
kowi.kowi <kowi.db.kowi@gmail.com>
2025-02-09 09:32:12 +0100

    Add mainn part

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world from PI!");
+use project1::run;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let argument = "127.0.0.1:0";
+    let listener = TcpListener::bind(argument).await?;
+    let std_listener = listener.into_std()?;
+    run(std_listener)?.await
 }
